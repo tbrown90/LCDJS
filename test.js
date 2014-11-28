a4cdfef8af009d60188fa38b85c1ba600eca4b32
Miskatonic Interactive <miskatonicinteractive@Miskatonics-iMac.local>
2014-11-28 10:52:07 -0600

    Adding test

---
+++ test.js
@@ -0,0 +1,16 @@
+var lcd = require('./index.js');
+var pin_rs = 22;
+var pin_e = 18;
+var pins_db = [16, 11, 13, 15];
+
+lcd.begin(16, 2);
+lcd.init(pin_rs, pin_e, pins_db);
+
+var date = new Date();
+var day = date.getDay();
+var month = (date.getMonth() + 1);
+var year = date.getFullYear;
+
+var dateStr = month + '-' + day + '-' + year;
+
+lcd.message(dateStr, true);

7e3d80239a840b92eec29073f8f2e54a56a76b79
Miskatonic Interactive <miskatonicinteractive@Miskatonics-iMac.local>
2014-11-28 12:47:49 -0600

    fix

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var pin_rs = 22;
 var pin_e = 18;
 var pins_db = [16, 11, 13, 15];
 
-lcd.init(pin_rs, pin_e, pins_db function() {
+lcd.init(pin_rs, pin_e, pins_db, function() {
     lcd.begin(16, 2);
 
     lcd.message('Hello World', true);

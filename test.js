ead48cffcdafc261895cde5723dfac1f227aa44c
Miskatonic Interactive <miskatonicinteractive@Miskatonics-iMac.local>
2014-11-28 12:03:09 -0600

    testing

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var lcd = require('./index.js');
 var pin_rs = 25;
 var pin_e = 24;
-var pins_db = [24, 17, 21, 22];
+var pins_db = [24, 17, 27, 22];
 
 lcd.begin(16, 2);
 lcd.init(pin_rs, pin_e, pins_db);

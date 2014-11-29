68eb3e6e40731c1202175625196e371a2aa1ea31
Miskatonic Interactive <miskatonicinteractive@Miskatonics-iMac.local>
2014-11-28 20:12:59 -0600

    adsfsdfdsfa

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ var pins_db = [12, 18, 22, 24];
 lcd.init(pin_rs, pin_e, pins_db, function() {
     lcd.begin(16, 2);
 
-    lcd.message('Hello World', true);
+    lcd.message('Good Bye Moon', true);
 
     setTimeout(lcd.cleanUp, 2000);
 });

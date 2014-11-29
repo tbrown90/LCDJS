3d0ebaf5d63e5e8f27b7353e1d9f74d052879a81
Miskatonic Interactive <miskatonicinteractive@Miskatonics-iMac.local>
2014-11-28 18:03:45 -0600

    asdfsdf

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 var lcd = require('./index.js');
+var sleep = require('sleep');
+
 var pin_rs = 22;
 var pin_e = 18;
 var pins_db = [16, 11, 13, 15];
@@ -8,6 +10,7 @@ lcd.init(pin_rs, pin_e, pins_db, function() {
 
     lcd.message('Hello World', true);
 
+    sleep.sleep(2);
     lcd.cleanUp();
 });
 

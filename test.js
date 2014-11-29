8287db255a73e60db21571e84a002afeb840f206
Miskatonic Interactive <miskatonicinteractive@Miskatonics-iMac.local>
2014-11-28 19:58:40 -0600

    asdfa

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var sleep = require('sleep');
 
 var pin_rs = 22;
 var pin_e = 18;
-var pins_db = [16, 11, 13, 15];
+var pins_db = [8, 11, 13, 15];
 
 lcd.init(pin_rs, pin_e, pins_db, function() {
     lcd.begin(16, 2);

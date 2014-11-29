e872549dd99dbbf8a99c6581225097b45d2e8f19
Miskatonic Interactive <miskatonicinteractive@Miskatonics-iMac.local>
2014-11-29 16:27:05 -0600

    adfasdf

--- test.js
+++ test.js
@@ -1,15 +1,17 @@
 var lcd = require('./index.js');
 var sleep = require('sleep');
 
-var pin_rs = 8;
-var pin_e = 10;
-var pins_db = [12, 18, 22, 24];
+var pin_rs = 14;
+var pin_e = 15;
+var pins_db = [18, 24, 25, 8];
 
 lcd.init(pin_rs, pin_e, pins_db, function() {
     lcd.begin(16, 2);
 
     lcd.message('Good Bye Moon', true);
 
-    setTimeout(lcd.cleanUp, 2000);
+    setTimeout(function() {
+        lcd.cleanUp();
+    }, 2000);
 });
 
